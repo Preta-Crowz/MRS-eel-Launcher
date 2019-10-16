5b6833e8326220e388c84c2b7b1c1ff1cee0edbc
Socio-path <doohee006@gmail.com>
2019-10-16 13:59:32 +0900

    Add caddy webserver

--- main_new.py
+++ main_new.py
@@ -1,12 +1,13 @@
 import sys
 import os
-import pycraft
-import pycraft.exceptions as pex
-from pycraft import authentication
+import minecraft
+import minecraft.exceptions as pex
+from minecraft import authentication
 from cefpython3 import cefpython as cef
+from subprocess import Popen
 
 def isTokenVaild():
-    auth_token = pycraft.AuthenticationToken(browser.ExecuteFunction('loadToken'))
+    auth_token = minecraft.AuthenticationToken(browser.ExecuteFunction('loadToken'))
     print(auth_token)
     return bool(auth_token.validate())
     
@@ -14,7 +15,7 @@ def login(mcid, mcpw , js_callback=None):
     try:
         if mcid == "" or mcpw == "":
             warn("Invalid ID or Password!")
-        auth_token = pycraft.authentication.AuthenticationToken()
+        auth_token = minecraft.authentication.AuthenticationToken()
         auth_token.authenticate(mcid, mcpw)
         username = auth_token.profile.name
         print('Logined to ' + username)
@@ -23,17 +24,20 @@ def login(mcid, mcpw , js_callback=None):
         return False
     global currToken
     currToken = auth_token.access_token
-    js_callback.Call([auth_token.profile.name, auth_token.client_token, auth_token.access_token],os.getcwd())
+    js_callback.Call([auth_token.profile.name, auth_token.client_token, auth_token.access_token])
 
 #sys.excepthook = cef.ExceptHook  # To shutdown all CEF processes on error
-cef.Initialize()
-browser = cef.CreateBrowserSync(url="file://{}/page/login.html".format(os.getcwd()),
-                      window_title="MRS Launcher",settings={'web_security_disabled':True})
+os.environ["pagedir"] = os.getcwd()+r'\page'
+server = Popen(["caddy","-conf","caddyfile"])
+cef.Initialize(settings={'cache_path':os.getcwd()+r'\cache'})
+browser = cef.CreateBrowserSync(url="http://localhost:12345/login.html",
+                      window_title="MRS Launcher")
 bindings = cef.JavascriptBindings()
 bindings.SetFunction('isTokenVaild',isTokenVaild)
 bindings.SetFunction('login',login)
 browser.SetJavascriptBindings(bindings)
 cef.MessageLoop()
+server.terminate()
 cef.Shutdown()
 
 
