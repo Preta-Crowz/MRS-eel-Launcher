0e41bc7f7fcb8ce3ad7032c98ac42c2b8c8b4626
doohee <doohee006@gmail.com>
2019-10-16 16:58:49 +0900

    I don't know what i edited

--- main_new.py
+++ main_new.py
@@ -1,21 +1,20 @@
 import sys
 import os
-import minecraft
-import minecraft.exceptions as pex
-from minecraft import authentication
+import pycraft
+import pycraft.exceptions as pex
+from pycraft import authentication
 from cefpython3 import cefpython as cef
 from subprocess import Popen
 
-def isTokenVaild():
-    auth_token = minecraft.AuthenticationToken(browser.ExecuteFunction('loadToken'))
-    print(auth_token)
+def isTokenVaild(token):
+    auth_token = authentication.AuthenticationToken(token[0],token[1],token[2])
     return bool(auth_token.validate())
     
 def login(mcid, mcpw , js_callback=None):
     try:
         if mcid == "" or mcpw == "":
             warn("Invalid ID or Password!")
-        auth_token = minecraft.authentication.AuthenticationToken()
+        auth_token = pycraft.authentication.AuthenticationToken()
         auth_token.authenticate(mcid, mcpw)
         username = auth_token.profile.name
         print('Logined to ' + username)
@@ -32,6 +31,7 @@ server = Popen(["caddy","-conf","caddyfile"])
 cef.Initialize(settings={'cache_path':os.getcwd()+r'\cache'})
 browser = cef.CreateBrowserSync(url="http://localhost:12345/login.html",
                       window_title="MRS Launcher")
+cef.WindowUtils.SetIcon
 bindings = cef.JavascriptBindings()
 bindings.SetFunction('isTokenVaild',isTokenVaild)
 bindings.SetFunction('login',login)
