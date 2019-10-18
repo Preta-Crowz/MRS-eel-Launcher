8765aaa85949a699faa841dde50acc80c762bad0
AlphaBs <ksi123456ab@naver.com>
2019-10-18 01:21:10 +0000

    add event.py (implmentation of python event pattern)

---
+++ event.py
@@ -0,0 +1,36 @@
+from inspect import signature
+
+class event:
+    def __init__(self):
+        self.funcs = list()
+
+    def add_handler(self, f):
+        if not callable(f):
+            raise ValueError("'f' must be a function type")
+        self.funcs.append(f)
+
+
+    def remove_handler(self, f):
+        if not callable(f):
+            raise ValueError("'f' must be a function type")
+        self.funcs.remove(f)
+
+
+    def clear_handler(self):
+        self.funcs.clear()
+
+
+    def fire(self, sender, e):
+        for f in self.funcs:
+            sig = signature(f)
+            counts = len(sig.parameters)
+            
+            if counts == 0:
+                f()
+            elif counts == 1:
+                f(e)
+            elif counts == 2:
+                f(sender, e)
+            else:
+                raise ValueError("handler must have no more than two parameters")
+
