bb1204b7d5797e41bcbb9d372e1705e44ca50710
AlphaBs <ksi123456ab@naver.com>
2019-10-21 23:35:50 +0900

    fix setting bug
    cannot load setting obj when json content is none

--- setting.py
+++ setting.py
@@ -11,29 +11,31 @@ default_setting = {
 setting_obj = None
 
 def load():
-    global setting_obj
+    global setting_obj, default_setting
     try:
         if os.path.isfile(launcher.path_setting):
             content = util.readfile(launcher.path_setting)
             setting_obj = json.loads(content)
+
+            if setting_obj is None:
+                setting_obj = default_setting
         else:
             setting_obj = default_setting
     except:
         setting_obj = default_setting
+    print(setting_obj)
 
 def get(key):
     global setting_obj
-
-    if setting_obj == None:
+    if setting_obj is None:
         load()
-
+    print(setting_obj)
     return setting_obj.get(key)
 
 
 def set(key, val):
     global setting_obj
-
-    if setting_obj == None:
+    if setting_obj is None:
         load()
 
     setting_obj[key] = val
@@ -42,4 +44,3 @@ def set(key, val):
 def save():
     global setting_obj
     util.writefile(launcher.path_setting, json.dumps(setting_obj))
-
